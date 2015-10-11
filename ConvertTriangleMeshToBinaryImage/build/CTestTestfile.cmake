66d627281a37a22667a6f77250ff34b3fffaf72c
AnqiBai <baianqi086@gamil.com>
2015-10-11 11:04:57 -0400

    Test run 1.

---
+++ ConvertTriangleMeshToBinaryImage/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/anqi/Airway/ConvertTriangleMeshToBinaryImage
+# Build directory: /home/anqi/Airway/ConvertTriangleMeshToBinaryImage/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(ConvertTriangleMeshToBinaryImageTest "/ConvertTriangleMeshToBinaryImage" "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/HeadMRVolume.mha" "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/torus.vtk" "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/Output.mha")
---
+++ ConvertTriangleMeshToBinaryImage/build/cmake_install.cmake
@@ -0,0 +1,67 @@
+# Install script for directory: /home/anqi/Airway/ConvertTriangleMeshToBinaryImage
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "/usr/local")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  ELSE(BUILD_TYPE)
+    SET(CMAKE_INSTALL_CONFIG_NAME "")
+  ENDIF(BUILD_TYPE)
+  MESSAGE(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+ENDIF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+
+# Set the component getting installed.
+IF(NOT CMAKE_INSTALL_COMPONENT)
+  IF(COMPONENT)
+    MESSAGE(STATUS "Install component: \"${COMPONENT}\"")
+    SET(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  ELSE(COMPONENT)
+    SET(CMAKE_INSTALL_COMPONENT)
+  ENDIF(COMPONENT)
+ENDIF(NOT CMAKE_INSTALL_COMPONENT)
+
+# Install shared libraries without execute permission?
+IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  SET(CMAKE_INSTALL_SO_NO_EXE "1")
+ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Runtime")
+  IF(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh/ConvertTriangleMeshToBinaryImage" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh/ConvertTriangleMeshToBinaryImage")
+    FILE(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh/ConvertTriangleMeshToBinaryImage"
+         RPATH "")
+  ENDIF()
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh" TYPE EXECUTABLE FILES "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/build/ConvertTriangleMeshToBinaryImage")
+  IF(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh/ConvertTriangleMeshToBinaryImage" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh/ConvertTriangleMeshToBinaryImage")
+    IF(CMAKE_INSTALL_DO_STRIP)
+      EXECUTE_PROCESS(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/ITKExamples/Core/Mesh/ConvertTriangleMeshToBinaryImage")
+    ENDIF(CMAKE_INSTALL_DO_STRIP)
+  ENDIF()
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Runtime")
+
+IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Code")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ITKExamples/Code/Core/Mesh/ConvertTriangleMeshToBinaryImage" TYPE FILE FILES
+    "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/Code.cxx"
+    "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/CMakeLists.txt"
+    )
+ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Code")
+
+IF(CMAKE_INSTALL_COMPONENT)
+  SET(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+ELSE(CMAKE_INSTALL_COMPONENT)
+  SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+ENDIF(CMAKE_INSTALL_COMPONENT)
+
+FILE(WRITE "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/build/${CMAKE_INSTALL_MANIFEST}" "")
+FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
+  FILE(APPEND "/home/anqi/Airway/ConvertTriangleMeshToBinaryImage/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+ENDFOREACH(file)
